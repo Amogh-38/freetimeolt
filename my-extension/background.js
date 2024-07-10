cc2e59a7259a8498f01262832f5814488f6ca2bd
unknown <amoghtiwari12a@gmail.com>
2024-07-10 10:31:13 +0530

    third commit

--- my-extension/background.js
+++
@@ -1,4 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('Question Array Modifier extension installed.');
-  });
-  
--- my-extension/popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('modify-button').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        function: modifyQuestions
-      });
-    });
-  });
-  
