20a2e87ea10803bef68b2831bebc3dd26a69fbad
unknown <amoghtiwari12a@gmail.com>
2024-07-10 10:00:13 +0530

    third commit

--- content.js
+++ content.js
@@ -1,25 +1,18 @@
-function modifyQuestions() {
-    if (typeof a === 'object' && typeof a.getQuestion === 'function') {
-      const questions = a.getQuestion(1);
-  
-      questions.forEach(question => {
-        if (Array.isArray(question) && question.length >= 8) {
-          const INDEX_FOR_FIRST_PROPERTY = 2;
-          const INDEX_FOR_SECOND_PROPERTY = 6;
-          const INDEX_FOR_THIRD_PROPERTY = 7;
-  
-          question[INDEX_FOR_FIRST_PROPERTY] = 1;
-          question[INDEX_FOR_SECOND_PROPERTY] = 3;
-          question[INDEX_FOR_THIRD_PROPERTY] = "answered";
-        } else {
-          console.error('Question array is not in the expected format:', question);
-        }
-      });
-    } else {
-      console.error('The expected object or method is not available.');
-    }
+(function() {
+  // Check if the 'a' object and 'getQuestion' function are available in the global scope
+  if (typeof a === 'object' && typeof a.getQuestion === 'function') {
+    const questions = a.getQuestion(1);
+
+    questions.forEach(i => {
+      if (Array.isArray(i) && i.length >= 8) {
+        i[2] = 1;
+        i[6] = 3;
+        i[7] = "answered";
+      } else {
+        console.error('Question array is not in the expected format:', i);
+      }
+    });
+  } else {
+    console.error('The expected object or method is not available.');
   }
-  
-  // Run the modification function
-  modifyQuestions();
-  
+})();
